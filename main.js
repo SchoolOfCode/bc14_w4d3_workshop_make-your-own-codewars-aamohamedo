f5de49f84d79ad3ae07fb770f8bfe6be71b8613c
Anas Mohamed <Anas Mohamed>
2023-04-10 14:43:51 +0100

    codewars

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 /* 
-👉 Write your kata here!
+
+Its the last day before eid, you've left work and need to do shopping in atleast 4 of the shops on 
+the list given to you by your partner.
+
+You have 15 mins in each shop. 
+
+create a function that returns "Well done, You're a great shopper!" if you made it to 4 or more shops.
+or returns "Sorry, You're a bad shopper!" if you didnt make it to a minimum of 4 shops.
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+
+
+export function howMuchTime(timeAvailable){
+    const timeInEachShop = 15;
+    const numberOfShops = Math.floor(timeAvailable / timeInEachShop); 
+
+    if (numberOfShops >= 4) {
+       return "Well Done, You're a good shopper! "
+    } else {
+        return "Sorry, you're a bad shopper!"
+    }
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,41 @@
 //👉 Write your tests below here:
+
+import { howMuchTime } from './main';
+
+import { test, expect} from '@jest/globals'; 
+
+
+// function howMuchTime(timeAvailable){
+//     const timeInEachShop = 15;
+//     const numberOfShops = Math.floor(timeAvailable / timeInEachShop); 
+
+//     if (numberOfShops >= 4) {
+//        return "Well Done, You're a good shopper! "
+//     } else {
+//         return "Sorry, you're a bad shopper!"
+//     }
+// }
+
+// test for time less that 60 mins 
+describe('findMessage', () => {
+
+test('Time less than 60 mins', function () {
+    const actual  = howMuchTime(30); //  2 shops
+    const expected = "Sorry, you're a bad shopper!"
+  expect(actual).toBe(expected);
+});
+
+// test for time equal to 60 mins
+test('Time equal to 60 mins', function () {
+    const actual = howMuchTime(60); // -- 4 shops 
+    const expected = "Well Done, You're a good shopper!"
+  expect(actual).toBe(expected);
+});
+
+// test for time 
+test('Time is more than 60 mins', function () {
+    const actual = howMuchTime(90); // -- 6 shops 
+    const expected = "Well Done, You're a good shopper!"
+  expect(actual).toBe(expected);
+});
+});
